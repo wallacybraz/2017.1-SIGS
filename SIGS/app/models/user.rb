0fe2395e207a3c1be6b3e123c2d1b2990c360e37
Bruno Matias <leguenta@hotmail.com>
2017-04-12 22:55:15 -0300

    Add column password_digest to controller users

--- SIGS/app/models/user.rb
+++ SIGS/app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
+<<<<<<< cb30e10e2013031ed0d867ab771ab314b22f2274
   has_one :coordinator, dependent: :destroy
   has_one :administrative_assistant, dependent: :destroy
   has_one :department_assistant, dependent: :destroy
+  has_secure_password
 end
